16e60cab98084dd8ff2dcfd1d5b5919497d5f504
Amnay, Amrita <amrita2@usf.edu>
2019-09-19 15:25:54 -0400

    Changes

--- Deliverable 3 - Methods/Program.cs
+++ Deliverable 3 - Methods/Program.cs
@@ -3,7 +3,6 @@
  Date: 19 September 2019
  Comments: This console asks users to enter thier first and last name and outputs the user's name plus a hello before the name.
  */
-
 using System;
 namespace Deliverable_3___Methods
 {
